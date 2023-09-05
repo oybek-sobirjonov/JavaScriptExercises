b2a4b99ffc6a6e4675fffe34bc1e2a9e174c0437
oybek-sobirjonov <87658070+oybek-sobirjonov@users.noreply.github.com>
2023-09-05 06:15:32 +0500

    Type conversions | Comparisions

---
+++ 2 lesson exercises/index.js
@@ -0,0 +1,30 @@
+/*
+// Type conversions
+console.log("" + 1 + 0); // 10 | string
+console.log("" - 1 + 0); // 1 | number
+console.log(true + false); // 1 | number
+console.log(6 / "3"); // 2 | number
+console.log("2" * "3"); // 6 | number
+console.log(4 + 5 + "px"); // 9px | string
+console.log("$" + 4 + 5); // $45 | string
+console.log("4" - 2); // 2 | number
+console.log("4px" - 2); // NaN | number
+console.log(" -9 " + 5); //  -9 5 | string
+console.log(" -9 " - 5); // -14 | number
+console.log(null + 1); // 1 | number
+console.log(undefined + 1); // NaN | number
+console.log(" \t \n" - 2); // -2 | number
+*/
+
+// Comparisions
+console.log(5 > 4); // true
+console.log("apple" > "pineapple"); /* false - because it compares the position
+of the letters on ASCII table and compares them, a = 97, p = 112; so a > p is false*/
+console.log("2" > "12") // true
+console.log(undefined == null); // true - both have no value
+console.log(undefined === null); // false - data type is different
+console.log(typeof undefined, typeof null); // undefined object
+console.log(null == "\n0\n"); // false
+console.log(0 == "\n0\n"); /* true - when comparing number to string JS automatically
+changes the data type of string to number and compares them*/
+console.log(null == +"\n0\n"); //false
