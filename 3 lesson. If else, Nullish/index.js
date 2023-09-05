d017f74d21687b4b962cc4eb7a24ac8752a28ed4
Oybek Sobirjonov <87658070+oybek-sobirjonov@users.noreply.github.com>
2023-09-05 08:22:00 +0500

    if else, nullish

---
+++ 3 lesson. If else, Nullish/index.js
@@ -0,0 +1,66 @@
+/*// Using if else construct, write the code which asks "What's the official name of JavaScript?"
+// If the visitor enters "ECMASript", then output "Right!", otherwise - output:
+// "You don't know? ECMAScript!"
+
+let input;
+
+if (input === "ECMASript") {
+    console.log("Right!");
+} else {
+    console.log("You don't know? ECMAScript!");
+}
+*/
+
+//=====================================
+/*
+// Assume that input is always number, if it is greater than 0 output - 1
+// if it is less than 0 output - -1, else output - 0
+
+let input;
+
+if (input > 0) {
+    console.log(1);
+} else if (input < 0) {
+    console.log(-1);
+} else {
+    console.log(0);
+}
+*/
+
+//=====================================
+/*
+// Rewrite this if using conditional operator
+ 
+let result;
+
+if (a + b < 4) {
+    result = "Below";
+} else {
+    result = "Over";
+}
+
+(a + b < 4) ? result = "Below" : result = "Over";
+*/
+
+//=====================================
+/*
+// Rewrite if else using multiple ternary operators '?'
+// For readability, it's recommended to split the code into multiple lines
+
+let message;
+
+if (login == "Employee") {
+    message = "Hello";
+} else if (login == "Director") {
+    message = "Greetings";
+} else if (login == "") {
+    message = "No login";
+} else {
+    message = "";
+}
+
+(login == "Employee") ? message = "Hello"
+: (login == "Director") ? message = "Greetings"
+: (login == "") ? message = "No login"
+: message = "";
+*/
