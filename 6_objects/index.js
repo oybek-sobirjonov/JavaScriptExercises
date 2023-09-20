96ae7a99f740b0609a5cbf672eeadab91cb0b28a
Oybek Sobirjonov <87658070+oybek-sobirjonov@users.noreply.github.com>
2023-09-20 09:20:35 +0500

    objects

---
+++ 6_objects/index.js
@@ -0,0 +1,37 @@
+// Problem 1 ====================================
+
+// print the key and values of the object below
+
+// const ac1 = {
+//     name: "Webbrain Academy",
+//     major: "Frontend",
+//     founded: 2022,
+//     students: 1000,
+//     mentros: 5
+// };
+
+// for(let i in ac1) {
+//     console.log(i, ac1[i]);
+// }
+
+// Problem 1 ====================================
+
+// Find the keys of the object below
+
+// let obj = {
+//     id: 1,
+//     name: "Webbrain"
+// };
+
+// for(let i in obj) {
+//     console.log(i);
+// }
+
+// Problem 2 ====================================
+
+// Find the value of the object which is a number
+
+let obj = {
+    id: 1,
+    name: "Webbrain"
+};
